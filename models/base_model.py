f518fd86fd1225c1827a039638899ccb37c9bd76
afarizap <afarizap@gmail.com>
2020-10-29 17:44:07 +0000

    add __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,10 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id,
+                                     self.__dict__)
     def save(self):
         self.updated_at = datetime.now()
 
