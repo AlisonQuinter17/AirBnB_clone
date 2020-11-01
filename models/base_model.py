cb042882fcef432f3e8b83f18b67ba73a36c2571
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-11-01 02:19:52 -0500

    Objs storage.

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Module for BaseModel class."""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel():
@@ -30,6 +31,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -45,6 +47,7 @@ class BaseModel():
         the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
