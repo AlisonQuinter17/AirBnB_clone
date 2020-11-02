380f39cdc6bd0a854404ca6cd99d01f085e882b2
afarizap <afarizap@gmail.com>
2020-11-02 23:52:31 +0000

    fromdict to obj

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 """ defines all common attributes/methods for other classes """
 from uuid import uuid4
-from datetime import datetime, timedelta
+from datetime import datetime as dt
 
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key in ['updated_at', 'created_at']:
+                    fromisoformat = dt.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, fromisoformat)
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = dt.now()
+            self.updated_at = dt.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,
@@ -17,7 +26,7 @@ class BaseModel:
                                      self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = dt.now()
 
     def to_dict(self):
         model = self.__dict__.copy()
