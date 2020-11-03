1310ebae9a3a573a8701821f7c9608cd2ff69276
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-11-02 23:54:26 -0500

    Command interpreter.

--- console.py
+++ console.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Module for HBNBCommand class."""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter."""
     prompt = "(hbnb) "
+    allowed_modules = ["BaseModel"]
 
     def do_quit(self, line):
         """Quit command to exit the program"""
@@ -20,7 +22,23 @@ class HBNBCommand(cmd.Cmd):
         """Empty line + ENTER shouldn’t execute anything."""
         pass
 
+    def do_create(self, line):
+        """
+        Creates a new instance of BaseModel, saves it
+        (to the JSON file) and prints the id.
+        """
+        if line:
+            args = line.split()
+
+            if args[0] not in self.allowed_modules:
+                print("** class doesn't exist **")
+            else:
+                if args[0] == self.allowed_modules[0]:
+                    instance = BaseModel()
+                    instance.save()
+                    print(instance.id)
+        else:
+            print("** class name missing **")
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
-
-    
