4c3e5f2014ace44ab9ba4525480f86541dcf9478
afarizap <afarizap@gmail.com>
2020-11-03 14:00:53 +0000

    add comments fix sheban

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ defines all common attributes/methods for other classes """
-from uuid import uuid4
 from datetime import datetime as dt
+from uuid import uuid4
 
 
 class BaseModel:
-
+    """ base model """
     def __init__(self, *args, **kwargs):
         if len(kwargs) > 0:
             for key, value in kwargs.items():
@@ -21,14 +21,17 @@ class BaseModel:
             self.updated_at = dt.now()
 
     def __str__(self):
+        """ print """
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id,
                                      self.__dict__)
 
     def save(self):
+        """ update """
         self.updated_at = dt.now()
 
     def to_dict(self):
+        """ dict """
         model = self.__dict__.copy()
         model["__class__"] = self.__class__.__name__
         model["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
